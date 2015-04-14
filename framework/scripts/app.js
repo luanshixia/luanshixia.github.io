bcb460bf0ac580b8ea9e960b09492821dc925ac7
luanshixia <dreambuild@qq.com>
2015-04-14 09:46:05 +0800

    Add a responsive template using Angular/Bootstrap

---
+++ framework/scripts/app.js
@@ -0,0 +1,10 @@
+﻿// define app and dependencies
+angular.module('app', ['ngRoute'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+        .when('/about',         { templateUrl: 'partials/about.html' })
+
+        .when('/home',         { templateUrl: 'partials/home.html' })
+        .when('/',             { templateUrl: 'partials/home.html' })
+        .otherwise({ redirectTo: '/' });
+}]);
---
+++ framework/scripts/controllers/appCtrl.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+
+// declare app module
+var app = angular.module('app');
+
+// app controller provides data
+app.controller('appCtrl', function appCtrl($scope, $filter) {
+
+    
+});
