1a6f09879e4631d16bf4a3939a909bca4503d93d
Yang Wang <luanshixia@gmail.com>
2018-07-25 12:07:56 -0700

    Test show SVG

--- js/emind-demo.js
+++ js/emind-demo.js
@@ -1,3 +1,5 @@
+import { Node, MindMap } from './emind.js';
+
 const root = Node.fromSpecString('{"content":"Root","children":[{"content":"Children 1","children":[{"content":"Children 4"},{"content":"Children 5"}]},{"content":"Children 2"},{"content":"Children 3"}]}');
 
 root.updateAllRecursively();
@@ -6,4 +8,4 @@ root.walk(node => console.log(node.toDebugString()));
 
 const map = new MindMap(root);
 
-document.getElementById('svg-container').innerHTML = map.getSvgString();
+document.getElementById('svg-container').innerHTML = map.toSvgString();
--- js/emind.js
+++ js/emind.js
@@ -1,205 +1,174 @@
 // emind.js
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
+export const hMargin = 100;
+export const vMargin = 10;
+export const cornerRadius = 5;
+export const cpOffset = 50;
+export const mapPadding = 100;
+export const textHeight = 15;
+export const nodePadding = 10;
+export const nodeClass = 'emind-node';
+export const linkClass = 'emind-link';
+export const textClass = 'emind-text';
+export function getBase62ShortID(length) {
+    const base62Chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    const chars = [];
+    for (let i = 0; i < length; i += 1) {
+        chars.push(base62Chars[Math.floor(Math.random() * 62)]);
     }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports"], factory);
+    return chars.join('');
+}
+export class Node {
+    constructor(content, ...children) {
+        this.width = 100;
+        this.height = 30;
+        this.totalHeight = 30;
+        this.left = 0;
+        this.top = 0;
+        this.content = content;
+        this.children = [];
+        children.forEach(node => this.addChild(node));
+        this.id = getBase62ShortID(8);
+        this.linkId = this.id + '-link';
+        this.textId = this.id + '-text';
     }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.hMargin = 100;
-    exports.vMargin = 10;
-    exports.cornerRadius = 5;
-    exports.cpOffset = 50;
-    exports.mapPadding = 100;
-    exports.textHeight = 15;
-    exports.nodePadding = 10;
-    exports.nodeClass = 'emind-node';
-    exports.linkClass = 'emind-link';
-    exports.textClass = 'emind-text';
-    function getBase62ShortID(length) {
-        var base62Chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-        var chars = [];
-        for (var i = 0; i < length; i += 1) {
-            chars.push(base62Chars[Math.floor(Math.random() * 62)]);
+    get right() {
+        return this.left + this.width;
+    }
+    get bottom() {
+        return this.top + this.height;
+    }
+    static fromSpecObject(specObject) {
+        const node = new Node(specObject.content, ...(specObject.children || []).map(child => Node.fromSpecObject(child)));
+        node.updateAllRecursively();
+        return node;
+    }
+    static fromSpecString(spec) {
+        const specObject = JSON.parse(spec);
+        return Node.fromSpecObject(specObject);
+    }
+    toSpecObject() {
+        return {
+            content: this.content,
+            children: this.children.length ? this.children.map(node => node.toSpecObject()) : undefined
+        };
+    }
+    toSpecString() {
+        return JSON.stringify(this.toSpecObject());
+    }
+    toDebugString() {
+        return `Content=${this.content}|Width=${this.width}|Height=${this.height}|TotalHeight=${this.totalHeight}|Left=${this.left}|Top=${this.top}`;
+    }
+    toSvgString() {
+        let link = '';
+        if (!this.isRoot()) {
+            const parent = this.parent;
+            const [x0, y0] = [this.left, this.top + this.height / 2];
+            const [x, y] = [parent.right, parent.top + parent.height / 2];
+            const [x1, y1] = [this.left - cpOffset, this.top + this.height / 2];
+            const [x2, y2] = [parent.right + cpOffset, parent.top + parent.height / 2];
+            link = `<path id="${this.linkId}" class="${linkClass}" d="M${x0} ${y0} C ${x1} ${y1}, ${x2} ${y2}, ${x} ${y}" stroke="black" fill="transparent" />`;
         }
-        return chars.join('');
+        return `<rect id="${this.id}" class="${nodeClass}" x="${this.left}" y="${this.top}" width="${this.width}" height="${this.height}" rx="${cornerRadius}" ry="${cornerRadius}" stroke="black" fill="transparent" /><text id="${this.textId}" class="${textClass}" x="${this.left + nodePadding}" y="${this.top + this.height / 2 + textHeight / 2}" text-anchor="left" font-size="${textHeight}">${this.content}</text>${link}`;
     }
-    exports.getBase62ShortID = getBase62ShortID;
-    var Node = /** @class */ (function () {
-        function Node(content) {
-            var children = [];
-            for (var _i = 1; _i < arguments.length; _i++) {
-                children[_i - 1] = arguments[_i];
-            }
-            var _this = this;
-            this.width = 100;
-            this.height = 30;
-            this.totalHeight = 30;
-            this.left = 0;
-            this.top = 0;
-            this.content = content;
-            this.children = [];
-            children.forEach(function (node) { return _this.addChild(node); });
-            this.id = getBase62ShortID(8);
-            this.linkId = this.id + '-link';
-            this.textId = this.id + '-text';
+    isRoot() {
+        return !this.parent;
+    }
+    isLeaf() {
+        return this.children.length === 0;
+    }
+    addChild(node) {
+        node.parent = this;
+        this.children.push(node);
+    }
+    walk(action) {
+        action(this);
+        this.children.forEach(node => node.walk(action));
+    }
+    updateWidth() {
+        this.width = 100;
+    }
+    updateHeight() {
+        this.height = 30;
+    }
+    updateAllRecursively() {
+        this.updateSizeRecursively();
+        this.updateTotalHeightRecursively();
+        this.updateLeftRecursively();
+        this.updateTopRecursively();
+    }
+    updateSizeRecursively() {
+        this.walk((node) => {
+            node.updateWidth();
+            node.updateHeight();
+        });
+    }
+    updateTotalHeightRecursively() {
+        this.children.forEach(node => node.updateTotalHeightRecursively());
+        if (this.isLeaf()) {
+            this.totalHeight = this.height;
         }
-        Object.defineProperty(Node.prototype, "right", {
-            get: function () {
-                return this.left + this.width;
-            },
-            enumerable: true,
-            configurable: true
-        });
-        Object.defineProperty(Node.prototype, "bottom", {
-            get: function () {
-                return this.top + this.height;
-            },
-            enumerable: true,
-            configurable: true
-        });
-        Node.fromSpecObject = function (specObject) {
-            var node = new (Node.bind.apply(Node, [void 0, specObject.content].concat((specObject.children || []).map(function (child) { return Node.fromSpecObject(child); }))))();
-            node.updateAllRecursively();
-            return node;
-        };
-        Node.fromSpecString = function (spec) {
-            var specObject = JSON.parse(spec);
-            return Node.fromSpecObject(specObject);
-        };
-        Node.prototype.toSpecObject = function () {
-            return {
-                content: this.content,
-                children: this.children.length ? this.children.map(function (node) { return node.toSpecObject(); }) : undefined
-            };
-        };
-        Node.prototype.toSpecString = function () {
-            return JSON.stringify(this.toSpecObject());
-        };
-        Node.prototype.toDebugString = function () {
-            return "Content=" + this.content + "|Width=" + this.width + "|Height=" + this.height + "|TotalHeight=" + this.totalHeight + "|Left=" + this.left + "|Top=" + this.top;
-        };
-        Node.prototype.toSvgString = function () {
-            var link = '';
-            if (!this.isRoot()) {
-                var parent_1 = this.parent;
-                var _a = [this.left, this.top + this.height / 2], x0 = _a[0], y0 = _a[1];
-                var _b = [parent_1.right, parent_1.top + parent_1.height / 2], x = _b[0], y = _b[1];
-                var _c = [this.left - exports.cpOffset, this.top + this.height / 2], x1 = _c[0], y1 = _c[1];
-                var _d = [parent_1.right + exports.cpOffset, parent_1.top + parent_1.height / 2], x2 = _d[0], y2 = _d[1];
-                link = "<path id=\"" + this.linkId + "\" class=\"" + exports.linkClass + "\" d=\"M" + x0 + " " + y0 + " C " + x1 + " " + y1 + ", " + x2 + " " + y2 + ", " + x + " " + y + "\" stroke=\"black\" fill=\"transparent\" />";
-            }
-            return "<rect id=\"" + this.id + "\" class=\"" + exports.nodeClass + "\" x=\"" + this.left + "\" y=\"" + this.top + "\" width=\"" + this.width + "\" height=\"" + this.height + "\" rx=\"" + exports.cornerRadius + "\" ry=\"" + exports.cornerRadius + "\" stroke=\"black\" fill=\"transparent\" /><text id=\"" + this.textId + "\" class=\"" + exports.textClass + "\" x=\"" + (this.left + exports.nodePadding) + "\" y=\"" + (this.top + this.height / 2 + exports.textHeight / 2) + "\" text-anchor=\"left\" font-size=\"" + exports.textHeight + "\">" + this.content + "</text>" + link;
-        };
-        Node.prototype.isRoot = function () {
-            return !this.parent;
-        };
-        Node.prototype.isLeaf = function () {
-            return this.children.length === 0;
-        };
-        Node.prototype.addChild = function (node) {
-            node.parent = this;
-            this.children.push(node);
-        };
-        Node.prototype.walk = function (action) {
-            action(this);
-            this.children.forEach(function (node) { return node.walk(action); });
-        };
-        Node.prototype.updateWidth = function () {
-            this.width = 100;
-        };
-        Node.prototype.updateHeight = function () {
-            this.height = 30;
-        };
-        Node.prototype.updateAllRecursively = function () {
-            this.updateSizeRecursively();
-            this.updateTotalHeightRecursively();
-            this.updateLeftRecursively();
-            this.updateTopRecursively();
-        };
-        Node.prototype.updateSizeRecursively = function () {
-            this.walk(function (node) {
-                node.updateWidth();
-                node.updateHeight();
-            });
-        };
-        Node.prototype.updateTotalHeightRecursively = function () {
-            this.children.forEach(function (node) { return node.updateTotalHeightRecursively(); });
-            if (this.isLeaf()) {
-                this.totalHeight = this.height;
-            }
-            else {
-                this.totalHeight = this.children.map(function (node) { return node.totalHeight; }).reduce(function (x, y) { return x + y; }, 0) + exports.vMargin * (this.children.length - 1);
-            }
-        };
-        Node.prototype.updateLeft = function () {
-            if (this.isRoot()) {
-                this.left = -this.width / 2;
-            }
-            else {
-                this.left = this.parent.right + exports.hMargin;
-            }
-        };
-        Node.prototype.updateLeftRecursively = function () {
-            this.walk(function (node) { return node.updateLeft(); });
-        };
-        Node.prototype.updateTopRecursively = function () {
-            if (this.isRoot()) {
-                this.top = -this.height / 2;
-                this.updateChildrenTopsRecursively();
-            }
-        };
-        Node.prototype.updateChildrenTopsRecursively = function () {
-            if (this.children.length > 0) {
-                var acumuTop = this.top + this.height / 2 - this.totalHeight / 2 + this.children[0].totalHeight / 2 - this.children[0].height / 2;
-                for (var i = 0; i < this.children.length; i++) {
-                    var node = this.children[i];
-                    node.top = acumuTop;
-                    node.updateChildrenTopsRecursively();
-                    if (i < this.children.length - 1) {
-                        var next = this.children[i + 1];
-                        acumuTop += node.totalHeight / 2 + next.totalHeight / 2 + node.height / 2 - next.height / 2;
-                        acumuTop += exports.vMargin;
-                    }
+        else {
+            this.totalHeight = this.children.map(node => node.totalHeight).reduce((x, y) => x + y, 0) + vMargin * (this.children.length - 1);
+        }
+    }
+    updateLeft() {
+        if (this.isRoot()) {
+            this.left = -this.width / 2;
+        }
+        else {
+            this.left = this.parent.right + hMargin;
+        }
+    }
+    updateLeftRecursively() {
+        this.walk((node) => node.updateLeft());
+    }
+    updateTopRecursively() {
+        if (this.isRoot()) {
+            this.top = -this.height / 2;
+            this.updateChildrenTopsRecursively();
+        }
+    }
+    updateChildrenTopsRecursively() {
+        if (this.children.length > 0) {
+            let acumuTop = this.top + this.height / 2 - this.totalHeight / 2 + this.children[0].totalHeight / 2 - this.children[0].height / 2;
+            for (let i = 0; i < this.children.length; i++) {
+                const node = this.children[i];
+                node.top = acumuTop;
+                node.updateChildrenTopsRecursively();
+                if (i < this.children.length - 1) {
+                    const next = this.children[i + 1];
+                    acumuTop += node.totalHeight / 2 + next.totalHeight / 2 + node.height / 2 - next.height / 2;
+                    acumuTop += vMargin;
                 }
             }
-        };
-        return Node;
-    }());
-    exports.Node = Node;
-    var MindMap = /** @class */ (function () {
-        function MindMap(root) {
-            this.root = root;
         }
-        MindMap.fromSpecString = function (spec) {
-            return new MindMap(Node.fromSpecString(spec));
-        };
-        MindMap.prototype.toSpecString = function () {
-            return this.root.toSpecString();
-        };
-        MindMap.prototype.toSvgString = function () {
-            var content = '';
-            this.root.walk(function (node) { return content += node.toSvgString(); });
-            var _a = this.getBoundingBox(), left = _a[0], top = _a[1], right = _a[2], bottom = _a[3];
-            _b = [left - exports.mapPadding, top - exports.mapPadding, right + exports.mapPadding, bottom + exports.mapPadding], left = _b[0], top = _b[1], right = _b[2], bottom = _b[3];
-            return "<svg width=\"" + (right - left) + "\" height=\"" + (bottom - top) + "\" viewBox=\"" + left + " " + top + " " + (right - left) + " " + (bottom - top) + "\" xmlns=\"http://www.w3.org/2000/svg\">" + content + "</svg>";
-            var _b;
-        };
-        MindMap.prototype.getBoundingBox = function () {
-            var _a = [this.root.left, this.root.top, this.root.right, this.root.bottom], left = _a[0], top = _a[1], right = _a[2], bottom = _a[3];
-            this.root.walk(function (node) {
-                left = Math.min(left, node.left);
-                top = Math.min(top, node.top);
-                right = Math.max(right, node.right);
-                bottom = Math.max(bottom, node.bottom);
-            });
-            return [left, top, right, bottom];
-        };
-        return MindMap;
-    }());
-    exports.MindMap = MindMap;
-});
+    }
+}
+export class MindMap {
+    static fromSpecString(spec) {
+        return new MindMap(Node.fromSpecString(spec));
+    }
+    constructor(root) {
+        this.root = root;
+    }
+    toSpecString() {
+        return this.root.toSpecString();
+    }
+    toSvgString() {
+        let content = '';
+        this.root.walk((node) => content += node.toSvgString());
+        let [left, top, right, bottom] = this.getBoundingBox();
+        [left, top, right, bottom] = [left - mapPadding, top - mapPadding, right + mapPadding, bottom + mapPadding];
+        return `<svg width="${right - left}" height="${bottom - top}" viewBox="${left} ${top} ${right - left} ${bottom - top}" xmlns="http://www.w3.org/2000/svg">${content}</svg>`;
+    }
+    getBoundingBox() {
+        let [left, top, right, bottom] = [this.root.left, this.root.top, this.root.right, this.root.bottom];
+        this.root.walk((node) => {
+            left = Math.min(left, node.left);
+            top = Math.min(top, node.top);
+            right = Math.max(right, node.right);
+            bottom = Math.max(bottom, node.bottom);
+        });
+        return [left, top, right, bottom];
+    }
+}
